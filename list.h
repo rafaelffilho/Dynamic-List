afef416a5089e8913da024dd7c9c5e1319c63ac6
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2016-11-18 12:03:33 -0200

    Rewrited the code from scratch

--- list.h
+++ list.h
@@ -1,78 +1,133 @@
-typedef struct inter products;
-struct inter {
+/*
+    * Lista Dinamicamente Encadeada
+
+    * Arquivo: main_functions.h
+    * Descri��o: Biblioteca com as fun��es do programa
+
+    * Codigo por:
+
+    * Adolpho F. Z. Piazza
+    * Matheus Nava
+    * Rafael F. Filho
+
+    * Algoritmos e Programa��o II, Rafael Ballotin
+    * Engenharia de Computa��o, 2016
+*/
+
+typedef struct prods {
     int code;
     float price;
-    products *next;
-};
+    struct prods *next_position;
+} Products;
 
-int tam;
-
-void read_file(products *p1, FILE **fp){
-
-    products *temp;
-    temp = malloc(sizeof(products));
-
-    tam = 0;
-
-    while (fread(temp, sizeof(products), 1, *fp) != NULL) {
-        tam++;
-    }
-
-    rewind(*fp);
-
-    for (int i = 0; i < tam; p1 = p1->next) {
-        fread(temp,sizeof(products), 1, *fp);
-        temp->next = p1->next;
-        p1->next = temp;
-        i++;
-    }
-    p1->next = NULL;
+//Inicia a lista, j� declara a proxima posi��o como NULL
+void initialize (Products *products_list) {
+    products_list->next_position = NULL;
 }
 
-products * initialyze(products *p1, FILE **fp){
-
-    int tam = 0;
-
-    *fp = fopen("products.dat", "r+b");
-
-    if(*fp == NULL)
-        *fp = fopen("products.dat", "w+b");
-
-    p1 = malloc(sizeof(products));
-
-    p1->code = 0;
-    p1->price = 0;
-    p1->next = NULL;
-
-    read_file(p1, fp);
-
-    return p1;
-}
-
-void insert (int code, float price, products *p1){
-   products *temp;
-   temp = malloc(sizeof(products));
-   temp->code = code;
-   temp->price = price;
-   temp->next = p1->next;
-   p1->next = temp;
-}
-
-void print_list (products *p1) {
-   if (p1 != NULL && p1->code != 0 && p1->price != 0){
-        printf ("\nCode: %d\nPrice: %.2f\n", p1->code, p1->price);
-        if (p1->next == NULL)
-            return;
-        print_list (p1->next);
-    } else if (p1->code == 0 && p1->price == 0){
-        print_list(p1->next);
+//Verificar se a lista est� vazia ou n�o
+int empty_list (Products *products_list) {
+    if (products_list->next_position == NULL) {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
-void save_to_file(products *p1, FILE **fp){
+//Inserir o item j� no come�o da lista
+void insert_item (Products *products_list) {
+    Products *new_product = (Products *) malloc(sizeof(Products));
 
-    for(int i = 0; i < tam-1; i++){
-        fwrite(p1, sizeof(products), 1, *fp);
-        p1 = p1->next;
+    printf("Codigo do produto: ");
+    scanf(" %d", &new_product->code);
+    printf("Preco do produto: ");
+    scanf(" %f", &new_product->price);
+
+    Products *last_product = products_list->next_position;
+    products_list->next_position = new_product;
+    new_product->next_position = last_product;
+}
+
+void show_itens (Products *products_list) {
+    if (empty_list(products_list)) {
+        printf("\n\nA lista esta atualmente vazia.\n\n");
+        return 0;
     }
+
+    Products *tmp = products_list->next_position;
+
+    while (tmp != NULL) {
+        printf("\nCodigo: %d\nPreco: %.2f\n", tmp->code, tmp->price);
+        tmp = tmp->next_position;
+    }
+}
+
+//Liberar o espa�o na memoria
+void clean_space (Products *products_list) {
+    if (!empty_list(products_list)) { //Se a lista n�o estiver vazia
+        Products *next, *current;
+
+        current = products_list->next_position; //Pega a posi��o atual de memoria
+
+        while (current != NULL) { //Enquanto n�o chegar na ultima posi��o
+            next = current->next_position;
+            free(current);
+            current = next;
+        }
+    }
+}
+
+void save_itens (Products *products_list) {
+    //FILE *fp = fopen("database.txt", "r+");
+    FILE *fp = fopen("products.dat", "r+b");
+
+    if(fp == NULL)
+        fp = fopen("products.dat", "w+b");
+
+    Products *tmp = products_list->next_position;
+
+    while (tmp != NULL) {
+        //fprintf(fp, "%d %.2f\n", tmp->code, tmp->price);
+        fwrite(tmp, sizeof(Products), 1, fp);
+        tmp = tmp->next_position;
+    }
+
+    fclose(fp);
+}
+
+//Na hora de fazer a inser��o do arquivo para o programa, ele vai fazer uma
+//adi��o no final da lista, e n�o no inicio
+void insert_item_from_stream (Products *products_list, Products *temp) {
+    Products *new_product = (Products *) malloc(sizeof(Products));
+    new_product->code = temp->code;
+    new_product->price = temp->price;
+    new_product->next_position = NULL;
+
+    if (empty_list(products_list)) {
+        products_list->next_position = new_product;
+    } else {
+        Products *tmp = products_list->next_position;
+
+        while (tmp->next_position != NULL) {
+            tmp = tmp->next_position;
+        }
+
+        tmp->next_position = new_product;
+    }
+}
+
+void load_itens (Products *products_list) {
+    int code;
+    float price;
+    //FILE *fp = fopen("database.txt", "r+");
+    FILE *fp = fopen("products.dat", "r+b");
+    rewind(fp);
+
+    Products *temp = malloc(sizeof(Products));
+
+    while (fread(temp, sizeof(Products), 1, fp) != NULL) {
+        insert_item_from_stream(products_list, temp);
+    }
+
+    fclose(fp);
 }
--- main.c
+++ main.c
@@ -1,39 +1,49 @@
+/*
+    * Lista Dinamicamente Encadeada
+
+    * Arquivo: main.c
+    * Descri��o: Estrutura principal do programa
+
+    * Codigo por:
+
+    * Adolpho F. Z. Piazza
+    * Matheus Nava
+    * Rafael F. Filho
+
+    * Algoritmos e Programa��o II, Rafael Ballotin
+    * Engenharia de Computa��o, 2016
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
-int main(int argc, char const *argv[]) {
+int main () {
 
-    FILE *fp;
-    products *p1;
+    Products *products_list = (Products *) malloc(sizeof(Products));
+    initialize(products_list);
+    load_itens(products_list);
 
-    int code;
-    int choice = 1;
-    float price;
-
-    p1 = initialyze(p1, &fp);
+    int control = 1, choice;
 
     do {
-
-        printf("Type the code: ");
-        scanf(" %d", &code);
-        printf("Type the price: ");
-        scanf(" %f", &price);
-
-        insert(code, price, p1);
-
-        printf("\n0 - exit ");
+        printf("2 - Adicionar produto\n1 - Listar items\n0 - Sair\n-> ");
         scanf(" %d", &choice);
 
-    } while(choice);
+        switch (choice) {
+            case 2:
+                insert_item(products_list);
+            break;
 
-    print_list(p1);
+            case 1:
+                show_itens(products_list);
+            break;
 
-    save_to_file(p1, &fp);
-
-    free(p1);
-
-    fclose(fp);
-
-    return 0;
+            case 0:
+                save_itens(products_list);
+                clean_space(products_list);
+                control = 0;
+            break;
+        }
+    } while (control);
 }
